f1a6c94460bdb812f8a63eda8fe85358aaff7df1
pritish0666 <pritishpatra06@gmail.com>
2025-11-03 22:45:38 +0530

    fix/styling fixed

--- src/components/project/project-detail.tsx
+++ src/components/project/project-detail.tsx
@@ -8,6 +8,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Rating } from "@/components/ui/rating";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   ExternalLink,
   Github,
